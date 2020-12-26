61930a7ab984b27e79e6a111371cb786a62dfb87
NAVG2000 <navg2000@gmail.com>
2020-12-25 23:17:30 -0500

    print dynamodb input

--- app/dbUtils.py
+++ app/dbUtils.py
@@ -12,6 +12,7 @@ def addDrug(name, df):
     client = boto3.client('dynamodb')
     df_as_dict = df.to_dict('records')
     final_dict = {'name': name, 'data': df_as_dict}
+    print(final_dict)
     try:
         print("start dynamo put")
         client.put_item(
