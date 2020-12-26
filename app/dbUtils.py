c472c911902cbe0f7f8d3e79aa06971388c09fa5
NAVG2000 <navg2000@gmail.com>
2020-12-25 23:46:54 -0500

    change dynamodb put

--- app/dbUtils.py
+++ app/dbUtils.py
@@ -1,5 +1,6 @@
 import boto3
 import pandas as pd
+import datetime
 
 
 def addDrug(name, df):
@@ -9,14 +10,15 @@ def addDrug(name, df):
         name (string): drug name as it appears in the dynamodb table 'RxTracker-DrugsTable'
         df (dataframe): dataframe that contains all the drug data.
     """
-    client = boto3.client('dynamodb')
+    table = boto3.client('dynamodb').Table('RxTracker-DrugsTable')
+    df["Week"] = df.apply(lambda row: row.strftime('%-y/%-m/%-d'), axis=1)
     df_as_dict = df.to_dict('records')
     final_dict = {'name': name, 'data': df_as_dict}
     print(final_dict)
     try:
         print("start dynamo put")
-        client.put_item(
-            TableName='RxTracker-DrugsTable', Item=final_dict, ConditionExpression='attribute_not_exists(name)')
+        table.put_item(Item=final_dict,
+                       ConditionExpression='attribute_not_exists(name)')
     except Exception as e:
         print("dynamo put failed")
         print(e)
